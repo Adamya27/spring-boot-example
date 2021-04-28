675378326618819783e540c8edd10aab9c050944
adamya2797 <adamya.sharma@wynk.in>
2021-04-28 23:39:52 +0530

    Revert "Spring-boot-crud-complete-project"
    This reverts commit d2152a27f3d6a6a1087914430e2275ac84704310.

--- src/main/java/com/Conference/springbootexample/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Conference.springbootexample.repositories;
-
-import com.Conference.springbootexample.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker,Long> {
-}
